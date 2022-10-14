711b78d9668af0a6ad2dd8064bf603ff34aca202
Marty <martygilbert@gmail.com>
2022-10-14 07:58:01 -0400

    Working with % as CLI.
    About to move to web accessible w/download

--- cli/email_stats.php
+++ cli/email_stats.php
@@ -4,25 +4,42 @@ define ('CLI_SCRIPT', true);
 
 require_once(__DIR__.'/../../../config.php');
 
+/*
+require_login();
+if (!has_capability('moodle/site:config', $sitecontext, ($USER->id))) {
+	echo "No capability ERROR";
+	echo "about to redirect";
+	$index = new moodle_url($CFG->wwwroot);
+	redirect($index, 'You do not have permission to generate this
+			report.', null, \core\output\notification::NOTIFY_ERROR);
+} else {
+	//header('Content-type: application/ms-excel');
+	//header('Content-Disposition: attachment; filename='.$filename;
+*/
 
-$digestforums = $DB->get_records('digestforum', null, '', 'id, name');
+	echo "Date,Forum,Num Opened,Not Opened,% Opened\n";
+	$digestforums = $DB->get_records('digestforum', null, '', 'id, name');
 
-foreach ($digestforums as $forum) {
-	$sql = 
-		'SELECT digestdate, digestforumid,
-		COUNT(case when numviews > 0 then 1 end) AS NumOpened, 
-		COUNT(case when numviews = 0 then 1 end) AS NotOpened 
-		FROM {digestforum_tracker} 
+
+	foreach ($digestforums as $forum) {
+		$sql = 
+			'SELECT digestdate, digestforumid,
+			COUNT(case when numviews > 0 then 1 end) AS NumOpened, 
+			COUNT(case when numviews = 0 then 1 end) AS NotOpened 
+				FROM {digestforum_tracker} 
 		WHERE digestforumid = ?
-		GROUP BY digestforumid, digestdate
-		ORDER BY digestdate DESC';
+			GROUP BY digestdate
+			ORDER BY digestdate DESC';
 
-	$opened = $DB->get_records_sql($sql, [$forum->id], 0, 20);
-	
-	foreach ($opened as $value) {
-		echo $value->digestdate.",".
-			$forum->name.",".
-			$value->numopened.",".
-			$value->notopened."\n";
+		$opened = $DB->get_records_sql($sql, [$forum->id], 0, 20);
+
+		foreach ($opened as $value) {
+			$pctOpened =  100 * ($value->numopened) / ($value->numopened + $value->notopened);
+			echo $value->digestdate.",".
+				$forum->name.",".
+				$value->numopened.",".
+				$value->notopened.",".
+				round($pctOpened, 2)."%\n";
+		}
 	}
-}
+//}
